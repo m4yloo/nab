fd96cba33ef12b3bca3f70afe0fb50f195e28d6c
MayloMayloo <maylomaylo768@gmail.com>
2026-05-12 21:29:58 +0200

    Add Vercel configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  distDir: 'docs',
   images: {
     unoptimized: true,
   },
   devIndicators: {
     buildActivity: false,
   },
-  basePath: '/nab',
-  assetPrefix: '/nab',
 };
 
 export default nextConfig;
