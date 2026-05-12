0915e5f6d53e15009097dc5e53c85900336c3024
MayloMayloo <maylomaylo768@gmail.com>
2026-05-12 21:17:03 +0200

    Simplify GitHub Pages deployment workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  distDir: 'out',
   images: {
     unoptimized: true,
   },
   devIndicators: {
     buildActivity: false,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/nab' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/nab' : '',
+  basePath: '/nab',
+  assetPrefix: '/nab',
 };
 
 export default nextConfig;
