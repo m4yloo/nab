03f6c98ebdc63ea8c4f072b3e5eed22bc92b73e0
MayloMayloo <maylomaylo768@gmail.com>
2026-05-12 21:59:23 +0200

    Fix script reference to load correct JavaScript file

--- app/layout.js
+++ app/layout.js
@@ -25,7 +25,7 @@ export default function RootLayout({ children }) {
       </head>
       <body>
         {children}
-        <script src="/app-local.js" async></script>
+        <script src="/app.js" async></script>
       </body>
     </html>
   );
